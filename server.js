4ee2773259d3b5ac70b8e0ec77890b5701fd7346
shubhamkhunt04 <shubhamkhunt08@gmail.com>
2021-04-20 09:42:31 +0530

    mongodb atlas url added

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const port = process.env.PORT || 5000;
 
 const mongoose = require("mongoose");
 mongoose
-  .connect("mongodb://127.0.0.1:27017/phonebook", {
+  .connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: false,
